a41def720456b92d02ff681f16308ce523669e09
tdreid <trevor.d.reid@gmail.com>
2018-08-07 03:43:45 +0000

    Parse and schedule jobs from env variable LIFEBOT_JOBS

--- lifebot.js
+++ lifebot.js
@@ -13,3 +13,35 @@ try {
 } catch (err) {
   logger.error(`Configuration file not loaded. ${err.message}`);
 }
+
+try {
+  const jobsList = process.env.LIFEBOT_JOBS || '';
+  if (jobsList) {
+    const jobs = jobsList.split(',').map(job => {
+      const arr = job.split('|');
+      if (arr.length === 2) {
+        return { name: arr[0], schedule: arr[1] };
+      } else {
+        logger.error(
+          `'${job}' did not split into exactly 2 elements. Entry skipped.`
+        );
+      }
+    });
+    logger.info(`Jobs ready: ${jobs.length}`);
+    jobs.forEach(job => {
+      try {
+        cron.scheduleJob(job.schedule, () => {
+          try {
+            require(job.name);
+          } catch (err) {
+            logger.error(err.message);
+          }
+        });
+      } catch (err) {
+        logger.error(err.message);
+      }
+    });
+  }
+} catch (err) {
+  logger.error(`Error parsing jobs list. ${err.message}`);
+}
